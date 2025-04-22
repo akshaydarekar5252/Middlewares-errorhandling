8a4cb3aee9cd810bd80ad49303be25001b6bc4c6
Akshay Darekar <akshaydarekar5252@gmail.com>
2025-04-23 02:27:40 +0530

    Middleware

--- app.js
+++ app.js
@@ -1,9 +1,65 @@
 const express = require('express');
 const app = express();
+const ExpressError = require('./ExpressError');
 
-app.use((req, res)=>{
-    console.log("i am a middleware function");
-    res.send("middleware finished");
+// app.use((req, res,next)=>{
+//     console.log("i am a middleware function");
+//    next();
+// });
+
+// app.use("/random", (req, res, next) => {
+//     console.log("i am a middleware function for random route");
+//     next();
+// });
+
+const checkToken = (req,res,next)=>{
+    let {token}= req.query;
+    if(token === "giveaccess"){
+        next();
+    }
+   throw new ExpressError(401,"access denied");
+}
+
+
+app.get("/api",checkToken,(req, res)=>{
+    res.send("data");
+})
+
+
+
+//error handling middleware
+
+app.get("/err",(req, res)=>{
+    abcd = abcd;
+});
+
+//Activity 
+
+app.get("/admin",(req,res)=>{
+    throw new ExpressError(403,"Access to forbidden");
+})
+
+app.use((err,req,res,next)=>{
+    let {status=500, message="some error occurred"}= err;
+   res.status(status).send(message);
+})
+// app.use((err,req,res,next)=>{
+//     console.log("_____ERROR2______");
+//     next(err);
+// })
+
+
+
+// //logger
+// app.use((req, res, next) => {
+//     req.time= new Date(Date.now()).toString();
+//     console.log(req.method , req.hostname,req.path , req.time);
+//     next();
+// });
+
+//404
+app.use((req,res)=>{
+    res.status(404).send("page not found");
 })
 
 app.get('/', (req, res) => {
@@ -12,9 +68,11 @@ app.get('/', (req, res) => {
 
 app.get("/random",()=>{
     console.log("i am a random page");
-})
+});
 
 
 app.listen(8080,(req, res)=>{
     console.log("Server is running on port 8080");
-})
+});
+
+
